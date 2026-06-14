499967e301fe96a12a9b144dda45cb233c2953cf
RianNovaes <riannovaes281@gmail.com>
2026-06-14 20:55:25 -0300

    Correção do nome da classe de Aprovacao_escolar para AprovacaoEscolar

--- exercicios-basicos/AprovacaoEscolar.java
+++ exercicios-basicos/AprovacaoEscolar.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Aprovacao_escolar {
+public class AprovacaoEscolar {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         
