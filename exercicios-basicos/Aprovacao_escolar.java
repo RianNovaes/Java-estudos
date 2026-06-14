ddfa8058e2e88c365fbc6ada62c71a9a44491506
RianNovaes <riannovaes281@gmail.com>
2026-06-14 19:44:51 -0300

    Adiciona Aprovacao_escolar com cálculo de média e parecer de aprovação utilizando double para armazenar e calcular media.

---
+++ exercicios-basicos/Aprovacao_escolar.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Aprovacao_escolar {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.nextLine();
+        
+        System.out.println("Digite a sua primeira nota: ");
+        double nota1 = sc.nextInt();
+        
+        System.out.println("Digite a sua segunda nota: ");
+        double nota2 = sc.nextInt();
+        
+        double media = (nota1 + nota2) / 2;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Média: " + media);
+
+        if (media >= 7) {
+            System.out.println("Você foi aprovado!!!");
+        } else if (media >= 5) {
+            System.out.println("Você ficou de recuperação...");
+        } else {
+            System.out.println("Você foi reprovado.");
+        }
+        sc.close();
+    }
+}
